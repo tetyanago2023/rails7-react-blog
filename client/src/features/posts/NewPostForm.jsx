d4a021c6618d6087c495083d40cef3de8e608c6b
go2023 <tetyanago2018@gmail.com>
2023-09-04 19:03:09 -0700

    Create new post page was refactored via service usage

--- client/src/features/posts/NewPostForm.jsx
+++ client/src/features/posts/NewPostForm.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {useNavigate} from "react-router-dom";
 import {API_URL} from "../../constants.js";
+import {createPost} from "../../services/postService.js";
 
 function NewPostForm(props) {
     const [title, setTitle] = useState("");
@@ -10,24 +11,15 @@ function NewPostForm(props) {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const postData = { title, body }
+        const postData = { title, body };
 
-        const response = await fetch(API_URL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(postData)
-        });
-
-        if (response.ok) {
-            const { id } = await response.json();
-            navigate(`/posts/${id}`)
-        } else {
-            console.log("An error occurred, response NOT OK!")
+        try {
+            const response = await createPost(postData);
+            navigate(`/posts/${response.id}`);
+        } catch (e) {
+            console.error("Failed to create post: ", e);
         }
-
-    }
+    };
 
     return (
         <div>
--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -20,6 +20,23 @@ async function fetchPost(id) {
     return response.json();
 }
 
+async function createPost(postData) {
+    const response = await fetch(`${API_URL}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+    });
+
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+
+    return response.json();
+}
+
+
 async function deletePost(id) {
     const response = await fetch(`${API_URL}/${id}`, {
         method: "DELETE",
@@ -35,4 +52,4 @@ async function deletePost(id) {
     return response.json();
 }
 
-export { deletePost, fetchAllPosts, fetchPost };
+export { createPost, deletePost, fetchAllPosts, fetchPost };
