6f9221f0844acd1686a59b0c6d3d2d1761a8cf0a
go2023 <tetyanago2018@gmail.com>
2023-09-23 15:50:12 -0700

    Tests for postService were created

--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -51,19 +51,18 @@ async function updatePost(id, postData) {
 
     return response.json();
 }
+//
 async function deletePost(id) {
     const response = await fetch(`${API_URL}/${id}`, {
         method: "DELETE",
     });
 
-    if (!response.ok) {
-        throw new Error(response.statusText);
-    }
     // 204 is No Content status
     if (response.status === 204) {
         return null;
     }
-    return response.json();
+
+    throw new Error(response.statusText);
 }
 
 export { createPost, deletePost, fetchAllPosts, fetchPost, updatePost };
---
+++ client/src/services/postService.test.js
@@ -0,0 +1,126 @@
+import fetchMock from "jest-fetch-mock";
+import {
+    fetchAllPosts,
+    fetchPost,
+    createPost,
+    updatePost,
+    deletePost,
+} from "./postService";
+import { API_URL} from "../constants.js";
+
+fetchMock.enableMocks();
+jest.mock("../constants", () => ({
+    API_URL: "http://your-test-api-url",
+}));
+
+describe("Post API service", () => {
+    beforeEach(() => {
+        fetchMock.resetMocks();
+    });
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    //index
+    test("fetches all posts", async() => {
+        const mockData = [{ id: 1, title: "Test Post", body: "Case" }];
+        fetch.mockResponseOnce(JSON.stringify(mockData));
+
+        const result = await fetchAllPosts();
+
+        expect(result).toEqual(mockData);
+    });
+
+    //show
+    test("fetches a single post", async () => {
+        const mockPostID = 1;
+        const mockData = { id: mockPostID, title: "Test Post", body: "Case" };
+        fetch.mockResponseOnce(JSON.stringify(mockData));
+
+        const result = await fetchPost(mockPostID);
+
+        expect(result).toEqual(mockData);
+    });
+
+    // New => renders the create action with a @post and some partial stuff.
+    //create
+    test("creates a new post", async () => {
+        const mockData = { title: "Test Post", body: "Case" };
+        fetch.mockResponseOnce(JSON.stringify(mockData));
+
+        const result = await createPost(mockData);
+
+        expect(result).toEqual(mockData);
+    });
+
+    //edit
+    //update
+    test("updates a post", async () => {
+        const mockPostID = 1;
+        const mockData = { id: mockPostID, title: "Test Post", body: "Case" };
+
+        fetch.mockResponseOnce(JSON.stringify(mockData));
+
+        const result = await updatePost(mockPostID, mockData);
+
+        expect(result).toEqual(mockData);
+    });
+
+    //delete
+    test("deletes a post", async () => {
+        const mockPostID = 1;
+        fetch.mockResponseOnce(null, { status: 204 });
+
+        const result = await deletePost(mockPostID);
+
+        expect(result).toEqual(null);
+    });
+
+    // This is the unhappy path zone
+    test("throws an error when the fetchAllPost response is not ok", async () => {
+        // Ensure that we are actually throwing an error in the service.
+        // We can do this by mocking the response to be not ok.
+        fetch.mockResponseOnce(JSON.stringify({}), { status: 500 });
+
+        // We expect the service to throw an error.
+        await expect(fetchAllPosts()).rejects.toThrow();
+    });
+
+    test("throws an error when the fetchPost response is not ok", async () => {
+        const mockPostID = 1;
+        fetch.mockResponseOnce(JSON.stringify({}), { status: 500 });
+
+        await expect(fetchPost(mockPostID)).rejects.toThrow();
+    });
+
+    test("throws an error when the createPost response is not ok", async () => {
+        const mockData = { title: "Test Post", body: "Case" };
+        fetch.mockResponseOnce(JSON.stringify({}), { status: 500 });
+
+        await expect(createPost(mockData)).rejects.toThrow();
+    });
+
+    test("throws an error when the updatePost response is not ok", async () => {
+        const mockPostID = 1;
+        const mockData = { id: mockPostID, title: "Test Post", body: "Case" };
+
+        fetch.mockResponseOnce(JSON.stringify({}), { status: 500 });
+
+        await expect(updatePost(mockPostID, mockData)).rejects.toThrow();
+    });
+
+    test("throws an error when the deletePost response is not ok", async () => {
+        const mockPostID = 1;
+        fetch.mockResponseOnce(JSON.stringify({}), { status: 500 });
+
+        await expect(deletePost(mockPostID)).rejects.toThrow();
+    });
+
+    // Delete throws an error if the response is not ok and not 204
+    test("throws an error when the deletePost response is 500", async () => {
+        const mockPostID = 1;
+        fetch.mockResponseOnce(JSON.stringify({}), { status: 500 });
+
+        await expect(deletePost(mockPostID)).rejects.toThrow();
+    });
+});
